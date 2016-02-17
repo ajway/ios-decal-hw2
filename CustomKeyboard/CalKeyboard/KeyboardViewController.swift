55616a9ae17d6ff609ea95f395479a32cb5fcefa
Alan Way <away236@gmail.com>
2016-02-17 01:47:10 -0800

    Finished HW2
    Sorry it was late. Have been spending all my time on CS170 midterm.

--- CustomKeyboard/CalKeyboard/KeyboardViewController.swift
+++ CustomKeyboard/CalKeyboard/KeyboardViewController.swift
@@ -11,6 +11,12 @@ import UIKit
 class KeyboardViewController: UIInputViewController {
 
     @IBOutlet var nextKeyboardButton: UIButton!
+    @IBOutlet var returnKeyboardButton: UIButton!
+    @IBOutlet var deleteKeyboardButton: UIButton!
+    @IBOutlet var bearKeyboardButton: UIButton!
+    @IBOutlet var catKeyboardButton: UIButton!
+    @IBOutlet var fishKeyboardButton: UIButton!
+    @IBOutlet var pigKeyboardButton: UIButton!
     
     var keyboardView: UIView!
 
@@ -23,6 +29,7 @@ class KeyboardViewController: UIInputViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         loadInterface()
+        loadTargets()
     }
 
     override func didReceiveMemoryWarning() {
@@ -45,7 +52,39 @@ class KeyboardViewController: UIInputViewController {
         view.addSubview(keyboardView)
         view.backgroundColor = keyboardView.backgroundColor
         nextKeyboardButton.addTarget(self, action: "advanceToNextInputMode", forControlEvents: .TouchUpInside) // advanceToNextInputMode is already defined in template
-        
+    }
+    
+    func loadTargets() {
+        returnKeyboardButton.addTarget(self, action:"returnAction", forControlEvents: .TouchUpInside)
+        deleteKeyboardButton.addTarget(self, action:"deleteAction", forControlEvents: .TouchUpInside)
+        bearKeyboardButton.addTarget(self, action:"bearAction", forControlEvents: .TouchUpInside)
+        catKeyboardButton.addTarget(self, action:"catAction", forControlEvents: .TouchUpInside)
+        fishKeyboardButton.addTarget(self, action:"fishAction", forControlEvents: .TouchUpInside)
+        pigKeyboardButton.addTarget(self, action:"pigAction", forControlEvents: .TouchUpInside)
+    }
+    
+    func returnAction() {
+        (textDocumentProxy as UIKeyInput).insertText("\n")
+    }
+    
+    func deleteAction() {
+        (textDocumentProxy as UIKeyInput).deleteBackward()
+    }
+    
+    func bearAction() {
+        (textDocumentProxy as UIKeyInput).insertText("ʕ⊙ᴥ⊙ʔ")
+    }
+    
+    func catAction() {
+        (textDocumentProxy as UIKeyInput).insertText("(^._.^)ﾉ")
+    }
+    
+    func fishAction() {
+        (textDocumentProxy as UIKeyInput).insertText(">ﾟ)))彡")
+    }
+    
+    func pigAction() {
+        (textDocumentProxy as UIKeyInput).insertText("[ ಠ (oo) ಠ ]")
     }
 
 
